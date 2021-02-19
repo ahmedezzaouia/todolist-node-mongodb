bc3d08250c29cf1cefe601d25dc8bef6db34b157
ahmedezzaouia <35857662+ahmedezzaouia@users.noreply.github.com>
2021-02-19 18:35:02 +0100

    ajhdj

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ app.get("/:customRoutName", function (req, res) {
 });
 
 let port = process.env.PORT;
-if (port === null || port === "") {
+if (port == null || port == "") {
   port = 3000;
 }
 app.listen(port, function () {
